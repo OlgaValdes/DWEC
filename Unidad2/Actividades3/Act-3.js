fdf8e59fd00e2e620b7f3128cb9e13f5cd156366
Olga Valdes <olgvalsan@alu.edu.gva.es>
2023-10-03 16:47:12 +0200

    Realizo actividad 3

---
+++ Unidad2/Actividades3/Act-3.js
@@ -0,0 +1,10 @@
+function CrearColorAleatorio() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.body.style.backgroundColor = CrearColorAleatorio();
